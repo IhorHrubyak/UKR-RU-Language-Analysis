f71a9031ff59725647ecc08f0c9cda069f296bef
John Ihor Campagna <johncampagna2@gmail.com>
2025-07-11 10:28:25 -0400

    Update

--- Main Code/Analyze Trends Data.R
+++ Main Code/Analyze Trends Data.R
@@ -1,4 +1,4 @@
-#Run queeires to get data, could maybe have loop of terms 
+#Run queries to get data, could maybe have loop of terms 
 #date for queries if not run 
 date <- "2010-01-01 2025-05-01"
 # trends_dat(what,"what","що таке","что такое","UA",date,what_t,what_r,what_c)
@@ -14,8 +14,15 @@ date <- "2010-01-01 2025-05-01"
 # trends_dat(money,"money","гроші","деньги","UA",date,date,region,city)
 # trends_dat(war,"war","війна","война","UA",date,date,region,city)
 
-#Perhaphs ideal to have
-# trends_dat(composite,"composite","що таке+новини+ігри+рецепти+Як+ціна+чому+гроші","что такое+Как+новости+игры+рецепты+цена+почему+деньги","UA",date,date,region,city)
+#Composite
+trends_dat(composite,"composite","що+новини+ігри+рецепти+Як+ціна+чому+гроші","что+Как+новости+игры+рецепты+цена+почему+деньги","UA",date,date,region,city)
+
+#By Region
+trends_by_regions(
+  keywords_ukrainian = "що+новини+ігри+рецепти+Як+ціна+чому+гроші",
+  keywords_russian = "что+Как+новости+игры+рецепты+цена+почему+деньги",date
+)
+
 
 #If data saved and want to load
 how <-  time_plot(wide_dat(process_g_trends(readRDS("C:/UKR-RU-Language-Analysis/Trend Queries/how2010_01_01_2025_05_01.RDS"))[[1]]), "How: Як/как")
@@ -24,12 +31,11 @@ what <-  time_plot(wide_dat(process_g_trends(readRDS("C:/UKR-RU-Language-Analysi
 price <-  time_plot(wide_dat(process_g_trends(readRDS("C:/UKR-RU-Language-Analysis/Trend Queries/recipes2010_01_01_2025_05_01.RDS"))[[1]]), "Price: Ціна/Цена")
 games <-  time_plot(wide_dat(process_g_trends(readRDS("C:/UKR-RU-Language-Analysis/Trend Queries/games2010_01_01_2025_05_01.RDS"))[[1]]), "Games: Ігри/Игри")
 recipes <-  time_plot(wide_dat(process_g_trends(readRDS("C:/UKR-RU-Language-Analysis/Trend Queries/recipes2010_01_01_2025_05_01.RDS"))[[1]]), "Recipes: Рецепти/Рецепты")
-
+composite <-  time_plot(wide_dat(process_g_trends(readRDS("C:/UKR-RU-Language-Analysis/Trend Queries/composite2010_01_01_2025_05_01.RDS"))[[1]]), "Composite")
 
 #Plot over date
 grid.arrange(how,News,what,price,games,recipes, nrow = 2,
              top="Graphs of Ukrainian/Russian Ratios over date")
-
 
 #Ideas have graph over time and map by oblast of ratio. Some sort of time series analysis to test if there are breaks in data. For regional analysis over time, could manually download
 how_region <- readRDS("C:/UKR-RU-Language-Analysis/Trend Queries/how2010_01_01_2025_05_01.RDS")[[3]]
@@ -39,7 +45,7 @@ reg_ru <- how_region  %>%
   filter(keyword=="Как") %>%
   arrange(location) %>% select(hits) 
 
-reg_ua <- corona_region  %>%
+reg_ua <- how_region  %>%
   filter(keyword=="Як") %>%
   arrange(location) %>% select(location,hits) 
 
@@ -47,6 +53,7 @@ reg_ua <- corona_region  %>%
 reg_ua <- rename(reg_ua,hits_ua=hits)
 reg_ru <- rename(reg_ru,hits_ru=hits)
 regions_tab <- cbind(reg_ua,reg_ru)
+
 #Setting Russian to numeric and sorting by it. Could be better to look at rus realtive to ukr as ratio?
 regions_tab$hits_ua <-  ifelse(regions_tab$hits_ua == "<1",0,regions_tab$hits_ua)
 regions_tab$hits_ua <-  as.numeric(ifelse(regions_tab$hits_ua == "",0,regions_tab$hits_ua))
@@ -58,7 +65,6 @@ regions_tab <-  arrange(regions_tab, -regions_tab$rat)
 names(regions_tab) <- c("Oblast", "Hits UA", "Hits RU", "Ratio UA/RU")
 stargazer(regions_tab,summary=FALSE,
           title= "Ukrainian Oblasts and Cities Ranked by Ukrainian/Russian Ratio", notes = "Crimea and Sevastapol had insignificant Ukrainian results",header =FALSE)
-
 
 
 regions_tab %>%
